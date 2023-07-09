190d69a36b56d7385857b9f6eb9e1cd2a164d65f
Ritsuki33 <intellpro0918@gmail.com>
2023-07-09 21:40:08 +0900

    シングルトン　実装

---
+++ Assets/Scripts/Common/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// シングルトン
+/// </summary>
+/// <typeparam name="TClass"></typeparam>
+public class Singleton<TClass> where TClass : class, new()
+{
+    static private TClass instance;
+
+    static public TClass Instance
+    {
+        get {
+
+            if (instance == null)
+            {
+                instance = new TClass();
+            }
+
+            return instance;
+        }
+    }
+
+    ~Singleton()
+    {
+        Dispose();
+    }
+
+    static public void Dispose()
+    {
+        instance = default;
+    }
+}
