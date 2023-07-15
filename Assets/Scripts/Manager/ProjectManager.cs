33d9ce848e89fa22a0a1e76801cd1ae4e31ac0e5
Ritsuki33 <intellpro0918@gmail.com>
2023-07-15 16:19:26 +0900

    プロジェクトマネージャー　構築

---
+++ Assets/Scripts/Manager/ProjectManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectManager : SingletonComponent<ProjectManager>
+{
+    public static bool IsActive
+    {
+        get
+        {
+            return Instance != null && Instance.isActive;
+        }
+    }
+
+    /// <summary>
+    /// アクティブかどうか
+    /// </summary>
+    private bool isActive = true;
+
+
+    /// <summary>
+    /// アクティブの切り替え
+    /// </summary>
+    /// <param name="flag"></param>
+    public void SetActive(bool flag)
+    {
+        isActive = flag;
+    }
+}
