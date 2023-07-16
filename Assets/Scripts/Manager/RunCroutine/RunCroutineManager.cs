04a905b861afc84cf5975419409c010b0b68de79
Ritsuki33 <intellpro0918@gmail.com>
2023-07-17 01:56:51 +0900

    RunCoroutineManager　実装

---
+++ Assets/Scripts/Manager/RunCroutine/RunCroutineManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class RunCroutineManager : Singleton<RunCroutineManager>
+{
+    GameObject gameObject = new GameObject();
+    RunCroutine runCroutine = default;
+    public  RunCroutineManager()
+    {
+        runCroutine = gameObject.AddComponent<RunCroutine>();
+    }
+
+    public void StartCroutine(IEnumerator croutine)
+    {
+        runCroutine.Run(croutine);
+    }
+}
